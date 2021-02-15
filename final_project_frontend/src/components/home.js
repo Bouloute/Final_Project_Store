c5de37b012389acc5eeb11e8abb9ce24ec5c2f87
Bouloute <annaick1995@gmail.com>
2021-02-15 16:06:51 -0500

    home page added info

--- final_project_frontend/src/components/home.js
+++ final_project_frontend/src/components/home.js
@@ -7,7 +7,12 @@ const home = () => {
             <br/><br/><br/>
                 Welcome to my Portfolio project<br/>
                 Here's a generic store website<br/>
-                This is a work in progress but feel free to look around. More things will be added soon<br/>
+                This is a work in progress but feel free to look around. More things will be added soon such as:<br/>
+                <ul>
+                    <li>Creating an account using social media or an email. With the use of OmniAuth gem.</li>
+                    <li>Being able to use the Contact.</li>
+                    <li>Seeing recipes with a search bar to find a recipe with a specific product.</li>
+                </ul>
             </p>
         </div>
     );
