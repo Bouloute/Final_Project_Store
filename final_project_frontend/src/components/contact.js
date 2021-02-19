9c08eaca8d72a84b643724a8ca9fbee5f2230f44
Bouloute <annaick1995@gmail.com>
2021-02-18 21:18:53 -0500

    contact form now send email

--- final_project_frontend/src/components/contact.js
+++ final_project_frontend/src/components/contact.js
@@ -1,64 +1,12 @@
 import React, { Component } from 'react';
+import emailjs from 'emailjs-com';
+import{ init } from 'emailjs-com';
 
 import '../contact.css'
+init("user_zYbmT4Wbs5YyKM730JPUb");
 
 class Contact extends Component {
-    render() {
-        return (
-            <div>
-            <section id="contact">
-                
-                <h1 className="section-header">CONTACT</h1>
-                
-                <div className="contact-wrapper">
-                    
-                    <form className="form-horizontal" >
-                    
-                        <div className="form-group">
-                            <div className="col-sm-12">
-                                <input type="text" className="form-control" id="name" placeholder="NAME" name="name" value=""/>
-                            </div>
-                        </div>
-
-                        <div className="form-group">
-                            <div className="col-sm-12">
-                            <input type="email" className="form-control" id="email" placeholder="EMAIL" name="email" value=""/>
-                            </div>
-                        </div>
-
-                        <textarea className="form-control" rows="10" placeholder="MESSAGE" name="message" ></textarea>
-                        
-                        <button className="btn btn-primary send-button" id="submit" type="submit" value="SEND">
-                            <div className="button">
-                                <span className="send-text">SEND</span>
-                            </div>
-                        </button>
-                    
-                    </form>
-                    
-                    
-                    <div className="direct-contact-container">
-
-                        <ul className="contact-list">
-                            <li className="list-item"><i className="fa fa-map-marker fa-2x"><span className="contact-text place">Somewhere, USA</span></i></li>
-                            
-                            <li className="list-item"><i className="fa fa-phone fa-2x"><span className="contact-text phone"><a href="tel:1-234-567-8901" title="Give me a call">(234) 567-8901</a></span></i></li>
-                            
-                            <li className="list-item"><i className="fa fa-envelope fa-2x"><span className="contact-text gmail"><a href="mailto:#" title="Send me an email">emailme@gmail.com</a></span></i></li>
-                        
-                        </ul>
-
-                        <hr/>
-
-                    </div>
-                    
-                </div>
-                
-                </section>  
-        </div>
-        );
-    }
-    /*constructor(props) {
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -73,9 +21,24 @@ class Contact extends Component {
     }
 
     handleSubmit = (event) => {
-        console.log("Form submitted")
         event.preventDefault();
-
+        //TODO make sure no fields are empty
+        emailjs.send("service_pohw0jg","template_juveuam",
+        {
+            from_name: this.state.name,
+            message: this.state.message,
+            reply_to: this.state.email,
+            }).then((result) => {
+                alert("Email sent")
+            }, (error) => {
+                alert("Error encountered, email not sent")
+            });
+            
+        this.setState({
+            name: "",
+            email: "",
+            message: ""
+        }) 
     }
 
     render() {
@@ -91,13 +54,13 @@ class Contact extends Component {
                     
                         <div className="form-group">
                             <div className="col-sm-12">
-                                <input type="text" className="form-control" id="name" placeholder="NAME" name="name" value="" onChange={this.handleChange}/>
+                                <input type="text" className="form-control" id="name" placeholder="NAME" name="name" value={this.state.name} onChange={this.handleChange}/>
                             </div>
                         </div>
 
                         <div className="form-group">
                             <div className="col-sm-12">
-                            <input type="email" className="form-control" id="email" placeholder="EMAIL" name="email" value="" onChange={this.handleChange}/>
+                            <input type="email" className="form-control" id="email" placeholder="EMAIL" name="email" onChange={this.handleChange}/>
                             </div>
                         </div>
 
@@ -132,7 +95,7 @@ class Contact extends Component {
                 </section>  
         </div>
         );
-    }*/
+    }
 }
 
 export default Contact;
--- final_project_frontend/src/components/home.js
+++ final_project_frontend/src/components/home.js
@@ -9,8 +9,8 @@ const home = () => {
                 Here's a generic store website<br/>
                 This is a work in progress but feel free to look around. More things will be added soon such as:<br/>
                 <ul>
-                    <li>Creating an account using social media or an email. With the use of OmniAuth gem.</li>
-                    <li>Being able to use the Contact.</li>
+                    <li>Creating an account using social media or an email. With the use of the OmniAuth gem.</li>
+                    <li>Being able to use the Contact form.</li>
                     <li>Seeing recipes with a search bar to find a recipe with a specific product.</li>
                 </ul>
             </p>
